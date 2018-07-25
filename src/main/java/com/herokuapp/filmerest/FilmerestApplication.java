4d8a8740c781dbdc694dd2af60c0537206822be6
José Ricardo <zevatron@zevatron.com.br>
2018-07-25 09:02:25 -0300

    SpringBoot start

---
+++ src/main/java/com/herokuapp/filmerest/FilmerestApplication.java
@@ -0,0 +1,12 @@
+package com.herokuapp.filmerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmerestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/herokuapp/filmerest/FilmerestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.herokuapp.filmerest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilmerestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
